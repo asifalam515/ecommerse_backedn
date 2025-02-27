605b7ebe9af612cae7aec77edc153c32a7063284
asifalam515 <asibulalam515@gmail.com>
2025-02-27 11:29:52 +0600

    search functionality added

--- src/app/modules/Product/product.controller.ts
+++ src/app/modules/Product/product.controller.ts
@@ -1,29 +1,31 @@
 import { Request, Response } from "express";
 import { ProductServices } from "./product.service";
+import ProductModel from "./product.model";
+import { TProduct } from "./product.interface";
 
 const createProduct = async (req: Request, res: Response) => {
   const { product } = req.body;
   const productData = await ProductServices.createProductToDB(product);
   res.status(200).json({
     succcess: true,
-    message: "product has been created",
+    message: "Product created successfully!",
     data: productData,
   });
 };
-const getProduct = async (req: Request, res: Response) => {
-  const products = await ProductServices.getProductFromDB();
-  res.status(200).json({
-    succcess: true,
-    message: "get all product",
-    data: products,
-  });
-};
+// const getProducts = async (req: Request, res: Response) => {
+//   const products = await ProductServices.getProductFromDB();
+//   res.status(200).json({
+//     succcess: true,
+//     message: "Products fetched successfully!",
+//     data: products,
+//   });
+// };
 const getSingleProduct = async (req: Request, res: Response) => {
   const { productId } = req.params;
   const product = await ProductServices.getSingleProductFromDB(productId);
   res.status(200).json({
     success: true,
-    message: "single product retrieve",
+    message: "Product fetched successfully!",
     data: product,
   });
 };
@@ -38,22 +40,56 @@ const updateProductData = async (req: Request, res: Response) => {
   );
   res.status(200).json({
     success: true,
-    message: "Product Updated successfully",
+    message: "Product updated successfully!",
     data: updatedProductData,
   });
 };
 const deleteProduct = async (req: Request, res: Response) => {
-  const { _id } = req.params;
-  await ProductServices.deleteProductFromDB(_id);
+  const { productId } = req.params;
+  await ProductServices.deleteProductFromDB(productId);
   res.status(200).json({
     success: true,
-    message: "deleted product",
+    message: "Product deleted successfully!",
   });
 };
+
+const searchProducts = async (req: Request, res: Response): Promise<any> => {
+  try {
+    const query: string | undefined = req.query.searchText as string;
+
+    if (!query) {
+      const products = await ProductServices.getProductFromDB();
+      return res.status(200).json({
+        success: true,
+        message: "Products fetched successfully!",
+        data: products,
+      });
+    }
+
+    // Search products if query is present
+    const results = await ProductModel.find({
+      name: { $regex: query, $options: "i" },
+    });
+
+    return res.status(200).json({
+      success: true,
+      message: "Products matching search term 'iphone' fetched successfully!",
+      data: results,
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: "Server error",
+      error: error instanceof Error ? error.message : "Unknown error",
+    });
+  }
+};
+
 export const productController = {
   createProduct,
-  getProduct,
+  // getProducts
   getSingleProduct,
   deleteProduct,
   updateProductData,
+  searchProducts,
 };
--- src/app/modules/Product/product.route.ts
+++ src/app/modules/Product/product.route.ts
@@ -1,12 +1,15 @@
 import express, { Router } from "express";
 import { productController } from "./product.controller";
+import ProductModel from "./product.model";
 
 const router: Router = express.Router();
 
 router.post("/products", productController.createProduct);
-router.get("/products", productController.getProduct);
+// router.get("/products", productController.getProducts);
+// router.get("products/:searchTerm", productController.searchProducts);
+router.get("/products", productController.searchProducts);
 router.get("/products/:productId", productController.getSingleProduct);
 router.put("/products/:productId", productController.updateProductData);
-router.delete("/products/:_id", productController.deleteProduct);
+router.delete("/products/:productId", productController.deleteProduct);
 
 export { router };
